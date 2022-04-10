e23e234d1fe601b958cc8262dd36da94ad415120
Javad <mjavad.jafari80@gmail.com>
2022-04-10 15:11:24 +0430

    Files changed

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-	//example cpp fil
+	// This file has been changed by Javad
+	int n;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; ++i) {
+		cin >> arr[i];
+	}
 }
